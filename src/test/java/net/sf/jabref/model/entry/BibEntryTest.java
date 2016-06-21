67b6deecd8732683a75ab5c3a0a0bc60cc143c73
Pedro Garcia <garcia.ph@gmail.com>
2016-06-20 22:38:05 -0300

    Update dos testes de validação

--- src/test/java/net/sf/jabref/model/entry/BibEntryTest.java
+++ src/test/java/net/sf/jabref/model/entry/BibEntryTest.java
@@ -1,6 +1,7 @@
 package net.sf.jabref.model.entry;
 
 import es2.trab.pedro.YearUtil;
+import org.junit.Assert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -30,49 +31,65 @@ public class BibEntryTest {
 
     @Test
     public void invalidArticleYearInput() {
+        String value = String.valueOf(YearUtil.OLDEST_POSSIBLE - 10);
         entry.setType("article");
-        entry.setField("year", String.valueOf(YearUtil.OLDEST_POSSIBLE - 10));
+        entry.setField("year", value);
+        Assert.assertFalse(entry.getField("year").equals(value));
     }
 
     @Test
     public void validArticleYearInput() {
+        String value = String.valueOf(YearUtil.OLDEST_POSSIBLE + 10);
         entry.setType("article");
-        entry.setField("year", String.valueOf(YearUtil.OLDEST_POSSIBLE + 10));
+        entry.setField("year", value);
+        Assert.assertTrue(entry.getField("year").equals(value));
     }
 
     @Test
     public void validArticleDOIInput() {
+        String value = "10.1000/xyz1000";
         entry.setType("article");
-        entry.setField("doi", "10.1000/xyz1000");
+        entry.setField("doi", value);
+        Assert.assertTrue(entry.getField("doi").equals(value));
     }
 
     @Test
     public void invalidArticleDOIInput() {
+        String value = "15.1000/xyz1000";
         entry.setType("article");
-        entry.setField("doi", "15.1000/xyz1000");
+        entry.setField("doi", value);
+        Assert.assertFalse(entry.getField("doi").equals(value));
     }
 
     @Test
     public void invalidBookYearInput() {
+        String value = String.valueOf(YearUtil.OLDEST_POSSIBLE - 10);
         entry.setType("book");
-        entry.setField("year", String.valueOf(YearUtil.OLDEST_POSSIBLE - 10));
+        entry.setField("year", value);
+        Assert.assertFalse(entry.getField("year").equals(value));
     }
 
     @Test
     public void validBookYearInput() {
+        String value = String.valueOf(YearUtil.OLDEST_POSSIBLE + 10);
         entry.setType("book");
-        entry.setField("year", String.valueOf(YearUtil.OLDEST_POSSIBLE + 10));
+        entry.setField("year", value);
+        Assert.assertTrue(entry.getField("year").equals(value));
     }
 
     @Test
     public void validBookDOIInput() {
+        String value = "10.1000/xyz1000";
         entry.setType("book");
-        entry.setField("doi", "10.1000/xyz1000");
+        entry.setField("doi", value);
+        Assert.assertTrue(entry.getField("doi").equals(value));
     }
 
     @Test
     public void invalidBookDOIInput() {
+        String value = "15.1000/xyz1000";
         entry.setType("book");
-        entry.setField("doi", "15.1000/xyz1000");
+        entry.setField("doi", value);
+        Assert.assertFalse(entry.getField("doi").equals(value));
     }
 }
